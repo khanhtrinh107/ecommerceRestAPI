b42bced43d72a777df83d361d91138ecf4f90aa6
khanhtrinh107 <khanhtrinh107@gmail.com>
2023-02-10 09:32:19 +0700

    'update-cart'

--- demo11/src/main/java/com/example/demo/entity/dto/Cart.java
+++ demo11/src/main/java/com/example/demo/entity/dto/Cart.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cart {
+    @NotNull(message = "id should not be null!" )
+    @NotBlank(message = "id should not be blank!")
     private Integer productId;
+    @NotNull(message = "id should not be null!" )
+    @NotBlank(message = "id should not be blank!")
     private String productName;
+    @Min(0)
     private float price;
+    @Min(0)
     private int quantity;
 }

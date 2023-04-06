6ac60c203da7f13c21ce4f369b62a027201d1349
khanhtrinh107 <khanhtrinh107@gmail.com>
2023-04-07 00:41:11 +0700

    create-logout-admin-page

--- demo11/src/main/java/com/example/demo/controller/view/Admin.java
+++ demo11/src/main/java/com/example/demo/controller/view/Admin.java
@@ -1,14 +1,28 @@
 package com.example.demo.controller.view;
 
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @PreAuthorize("hasRole('ADMIN')")
 public class Admin {
+    @Autowired
+    private UserService userService;
     @GetMapping("/view/admin")
-    public String admin(){
+    public String admin(Model model){
+        String res = (SecurityContextHolder.getContext().getAuthentication().getName());
+        if(res.equals("anonymousUser"))
+            res = null;
+        else{
+            int userId = userService.findByUserName(res).getUserId();
+            model.addAttribute("username" , res);
+            model.addAttribute("userId" , userId);
+        }
         return "admin";
     }
 }
